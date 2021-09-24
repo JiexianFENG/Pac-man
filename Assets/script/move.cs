9ede252dad9d3840c38d45832058d748f084232c
JiexianFENG <sherlockjiexian@gmail.com>
2021-09-25 03:04:32 +1000

    change the speed of the animation

--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class move : MonoBehaviour
 {
     // Start is called before the first frame update
-    Vector3 startP = new Vector3(-1.5f, 10.5f, 0);
-    Vector3 endP = new Vector3(-12.5f, 10.5f, 0);
+    Vector3 startP = new Vector3(-1.5f, 9.5f, 0);
+    Vector3 endP = new Vector3(-12.5f, 9.5f, 0);
     public Animator PacMan;
     void Start()
     {
@@ -32,29 +33,29 @@ public class move : MonoBehaviour
     Vector3 changeEndPoint(Vector3 oldEndPoint, Vector3 location)
     {
         float distances = Vector3.Distance(PacMan.transform.position, oldEndPoint);
-        if (distances <= 0.1f && oldEndPoint == new Vector3(-1.5f, 10.5f, 0))
+        if (distances <= 0.1f && oldEndPoint == new Vector3(-1.5f, 9.5f, 0))
         {
             PacMan.transform.SetPositionAndRotation(oldEndPoint,Quaternion.identity);
             PacMan.SetTrigger("left");
-            return new Vector3(-12.5f, 10.5f, 0);
+            return new Vector3(-12.5f, 9.5f, 0);
         }
-        else if (distances <= 0.1f && oldEndPoint == new Vector3(-12.5f, 10.5f, 0))
+        else if (distances <= 0.1f && oldEndPoint == new Vector3(-12.5f, 9.5f, 0))
         {
             PacMan.transform.SetPositionAndRotation(oldEndPoint, Quaternion.identity);
             PacMan.SetTrigger("up");
-            return new Vector3(-12.5f, 14.5f, 0);
+            return new Vector3(-12.5f, 13.5f, 0);
         }
-        else if (distances <= 0.1f && oldEndPoint == new Vector3(-12.5f, 14.5f, 0))
+        else if (distances <= 0.1f && oldEndPoint == new Vector3(-12.5f, 13.5f, 0))
         {
             PacMan.transform.SetPositionAndRotation(oldEndPoint, Quaternion.identity);
             PacMan.SetTrigger("right");
-            return new Vector3(-1.5f, 14.5f, 0);
+            return new Vector3(-1.5f, 13.5f, 0);
         }
         else
         {
             PacMan.transform.SetPositionAndRotation(oldEndPoint, Quaternion.identity);
             PacMan.SetTrigger("down");
-            return new Vector3(-1.5f, 10.5f, 0);
+            return new Vector3(-1.5f, 9.5f, 0);
         }
     }
     
